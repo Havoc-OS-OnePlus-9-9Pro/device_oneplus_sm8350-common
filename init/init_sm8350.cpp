66f9865275294e60ae8cbff082d25148ffe0d418
chandu078 <chandudyavanapelli03@gmail.com>
2021-04-08 14:36:12 +0530

    sm8350-common: libinit: Add proper model detection for OP9/9Pro
    Co-authored-by: Chris <cjh1249131356@gmail.com>
    Co-authored-by: HELLBOY017 <abhaygill017@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- init/init_sm8350.cpp
+++ init/init_sm8350.cpp
@@ -31,6 +31,7 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/sysinfo.h>
 #include <sys/system_properties.h>
 #include <sys/_system_properties.h>
@@ -79,6 +80,82 @@ void load_dalvikvm_properties() {
 }
 
 void vendor_load_properties() {
+  std::string project_codename = android::base::GetProperty("ro.boot.project_codename", "");
+  int rf_version = stoi(android::base::GetProperty("ro.boot.rf_version", ""));
+  if(project_codename == "lemonade"){
+      /* OnePlus 9 */
+      switch (rf_version){
+        case 11:
+          /* China */
+          property_override("ro.product.model", "LE2110");
+          break;
+        case 13:
+          /* India */
+          property_override("ro.product.model", "LE2111");
+          break;
+        case 21:
+          /* Europe */
+          property_override("ro.product.model", "LE2113");
+          break;
+        case 22:
+          /* Global / US Unlocked */
+          property_override("ro.product.model", "LE2115");
+          break;
+        default:
+          /* Generic */
+          property_override("ro.product.model", "LE2115");
+          break;
+      }
+  } else if(project_codename == "lemonadet"){
+      /* OnePlus 9 T-Mobile */
+      switch (rf_version){
+        case 12:
+          /* T-Mobile */
+          property_override("ro.product.model", "LE2117");
+          break;
+        default:
+          /* Generic */
+          property_override("ro.product.model", "LE2115");
+          break;
+      }
+  } else if(project_codename == "lemonadep"){
+      /* OnePlus 9 Pro */
+      switch (rf_version){
+        case 11:
+          /* China */
+          property_override("ro.product.model", "LE2120");
+          break;
+        case 13:
+          /* India */
+          property_override("ro.product.model", "LE2121");
+          break;
+        case 21:
+          /* Europe */
+          property_override("ro.product.model", "LE2123");
+          break;
+        case 22:
+          /* Global / US Unlocked */
+          property_override("ro.product.model", "LE2125");
+          break;
+        default:
+          /* Generic */
+          property_override("ro.product.model", "LE2125");
+          break;
+      }
+  } else if(project_codename == "lemonadept"){
+      /* OnePlus 9 Pro T-Mobile */
+      switch (rf_version){
+        case 12:
+          /* T-Mobile */
+          property_override("ro.product.model", "LE2127");
+          break;
+        default:
+          /* Generic */
+          property_override("ro.product.model", "LE2125");
+          break;
+      }
+  }
+
   // dalvikvm props
   load_dalvikvm_properties();
 }
